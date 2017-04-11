89f320e3fc5091b05561bec4747a129310931636
acrunchberry <jasong84@gmail.com>
2017-04-11 10:38:07 -0500

    finished files for this exercise

--- dayEleven/js/while_loop.js
+++ dayEleven/js/while_loop.js
@@ -20,13 +20,16 @@
 // console.log(links);
 
 // This is how you get a random number between 50 and 100
-var allCones = Math.floor(Math.random() * 50) + 50;
-console.log(allCones);
-var cones;
+// var allCones = Math.floor(Math.random() * 50) + 50;
+// console.log(allCones);
+// var cones;
 
-do {
-    cones = Math.floor(Math.random() * 5) + 1;
-    allCones = allCones - cones;
-    console.log(allCones);
-} while (allCones >= 0 && cones <= allCones)
-console.log("All Cones sold!");
+// do {
+//     cones = Math.floor(Math.random() * 5) + 1;
+//     allCones = allCones - cones;
+//     console.log(allCones);
+// } while (allCones >= 0 && cones <= allCones)
+// console.log("All Cones sold!");
+var conesInventory = Math.floor(Math.random() * 50) + 50;
+var conesRequested;
+var
