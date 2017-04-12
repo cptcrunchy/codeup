3a2ac68f47de44de4c9396a2fbade5f890f5be3d
acrunchberry <jasong84@gmail.com>
2017-04-12 14:26:04 -0500

    Updated iterating.js file

--- dayThirteen/iterating.js
+++ dayThirteen/iterating.js
@@ -3,16 +3,32 @@
 
     // TODO: Create an array of 4 people's names using literal array notation, in a variable called 'names'.
     var names = ['John', 'Dusty', 'Ean', 'Jason'];
-    // TODO: Create a log statement that will log the number of elements in the names array.
+    var meals = ['Lemon Pepper Talapia', 'Mac-n-Cheese', 'Cheeseburger', 'Chicken Soup', 'Fruit Plate',
+        'Fries', 'Chicken Wings', 'Hot Dog', 'Chimichanga', 'Burrito', 'Quinoa', 'Lentils-n-Rice'
+    ];
+    var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+        // TODO: Create a log statement that will log the number of elements in the names array.
     console.log('There are ' + names.length + ' names in the array.');
     // TODO: Create log statements that will print each of the names array elements individually.
     console.log("For Loop:");
     for (var i = 0; i < names.length; i++) {
         console.log('Name at index: ' + i + ' is ' + names[i]);
     }
-
+    console.log("");
     console.log("ForEach Loop:");
     names.forEach(function(element, index, array) {
         console.log('Name at index: ' + index + ' is ' + element);
     });
+
+    console.log("");
+    console.log("Random Meal on Day:");
+    var mealPicker = function() {
+        var todaysMeal = meals[Math.floor(Math.random() * 6)];
+        return todaysMeal;
+    }
+
+    for (i = 0; i < days.length; i++) {
+        console.log(days[i] + " -" + mealPicker());
+    }
+
 })();
