69792212cd4a1c36b1bcb4ae862fd15455bbebdb
acrunchberry <jasong84@gmail.com>
2017-04-22 02:28:40 -0500

    Recompiled necessary files for konami jQuery plugin

---
+++ dayNineteen/js/cheat.js
@@ -0,0 +1,11 @@
+(function($) {
+    "use strict";
+    $(function() {
+        $(window).konami({
+            cheat: function() {
+                    window.location.href = 'galaga.html';
+                } // end cheat
+        });
+        $(window).on('', function(event, opts) {});
+    });
+}(jQuery));
