d6e861f372645acadc2a91145e60382d8fc76ae7
acrunchberry <jasong84@gmail.com>
2017-04-26 15:37:29 -0500

    Initial setup of blog page

---
+++ dayTwentyOne/js/ajax-blog.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+
+            "use strict";
+
+            var url = "data/blog.json";
+            var options = {
+                "data": {
+                    title: "title",
+                    categories: "categories",
+                    quantity: "quantity",
+                    price: "price"
+                }
+            }
+
+            function writeNames(data) {
+                data.forEach(function(el) {
+                    var html = '<tr><td>' +
+                        el.title + '</td><td>' +
+                        el.quantity + '</td><td>$' +
+                        el.price + '</td><td>' +
+                        el.categories[0] + '</td></tr>'
+
+                    $('#insertProducts').append(html);
+
+                });
+            }
+
+
+            $.ajax(url).done(function(data) {
+                writeNames(data);
+            });
